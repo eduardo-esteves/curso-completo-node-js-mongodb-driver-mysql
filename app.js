65ad7b1f1b0425b0affccc4b3892001401adb72d
Esteves <esteves@edusites.com.br>
2018-04-17 20:57:12 -0300

    Instalado modulo EJS para separar os arquivos de html em pasta como 'view' fazendo uma separação entre a lógica da aplicação e seus templates de view. Exportado a view engine do ejs através do método set() do objeto express, isso é necessario sempre que necessitamos configurar um módulo para trabalhar em conjunto com o express

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 // Exporta o objeto express já executando-a
 const express = require('express')();
+/**
+ * Informando ao módulo Express que agora a engine
+ * de views será fornecida pelo módulo EJS sempre
+ * que necessitamos configurar um módulo para trabalhar
+ * em conjunto com o express, isso se dará pela propriedade set
+ */
+express.set('view engine', 'ejs');
 
 express.get('/', (req, resp) => {
   resp.send("<html><body><p>Noticias de Tecnologia!</p></body></html>");
 }); 
-
+// Agora o express pode fazer uso do método render() do EJS
+// organizando melhor os arquivos em um padrão MVC
 express.get('/tecnologia', (req, resp) => {
-  resp.send("<html><body><p>Bem vindo a página principal!</p></body></html>");
+  resp.render("secao/tecnologia");
 }); 
 
 express.listen(4200, () => {
