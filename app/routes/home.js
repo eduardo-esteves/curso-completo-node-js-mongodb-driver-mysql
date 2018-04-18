e21c0aa65ecff09a6d36ae0f80da9f41c19f5416
Esteves <esteves@edusites.com.br>
2018-04-18 11:30:21 -0300

    Setado os novos caminhos onde ficarão as views, já que antes estava na pasta views localizado na raiz e agora foi movido para dentro da pasta app, isso foi informado pelo novo modulo de configuração do express server.js. Também foi criado os arquivos de rotas dentro da pasta routes, cada arquivo desse representa um modulo de rota que nada mais são as funções de rotas do express que antes estava no app.js e esta sendo exportado para o app no padrão CommonJS por isso as funções de rota get() foram encapsulada por uma função anonima e em seguida exportada com module.exports para que na hora em que esses modulos forem importados pelo app.js com seus devidos require já serem executadas no app e terem suas rotas definidas e escutando as rotas no express.

---
+++ app/routes/home.js
@@ -0,0 +1,11 @@
+/* Feito o encapsulamento da função get() do express
+para o padrão de exportação do CommonJS para que
+na hora em que esse módulo for exportado já executar
+essa função de rota get(), sem este padrão de encapsulamento
+esse metodo não seria executado e deixaria de responder a 
+requisição. */ 
+module.exports = express => {
+  express.get('/', (req, resp) => {
+    resp.render('home/index');
+  });
+};
