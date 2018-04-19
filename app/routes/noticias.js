610f5a9412cdcd8a313b20981b712557920f724b
Esteves <esteves@edusites.com.br>
2018-04-19 17:36:58 -0300

    Após ter criado a consulta SQL adicionei um segundo parametro ao método render() do EJS no formato de um objeto JSON sendo o aliás que representará o valor na view e o valor da query retornada em um JSON pela função query(). então seguindo o primeiro parametro do método do EJS render('noticias/noticias') temos uma rota 'noticias' representada por uma pasta dentro do diretório de views 'views/noticias/noticias.ejs' e o arquivo de view noticias.ejs que renderiza a página e objetos JavaScript

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -3,7 +3,7 @@ para o padrão de exportação do CommonJS para que
 na hora em que esse módulo for exportado já executar
 essa função de rota get(), sem este padrão de encapsulamento
 esse metodo não seria executado e deixaria de responder a 
-requisição. */ 
+requisição. */
 module.exports = express => {
   express.get('/noticias', (req, resp) => {
     // feito importação do modulo mysql que está dentro de node_modules
@@ -18,9 +18,12 @@ module.exports = express => {
       }
     );
     // Cria um query SQL e a executa
-    conexao.query('SELECT * FROM noticias', (error, result) =>{
-      resp.send(result);
+    conexao.query('SELECT * FROM noticias', (error, result) => {
+      // resp.send(result);
+      resp.render("noticias/noticias", {
+        noticias: result
+      });
     });
-    //resp.render("noticias/noticias");
+
   });
 };
